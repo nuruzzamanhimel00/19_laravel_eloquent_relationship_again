1ff46580284cbcafc4b412eb82313043b58f6302
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2022-07-14 21:20:57 +0600

    commited

--- routes/web.php
+++ routes/web.php
@@ -218,7 +218,7 @@ Route::get('/tut_9_post_tag_create', function () {
 
     // ** secound way .............................
 
-    $post = Post::inRandomOrder()->first();
+    $post = Post::first();
     $tag = Tag::create([
         'name' => 'tag 12'
     ]);
